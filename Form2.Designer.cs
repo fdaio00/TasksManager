72df5dab6c3412154054beb00fe66693e972a758
fedaa raadan <feadaraadan@gmail.com>
2024-08-05 03:22:04 +0300

    adding tab contorl and menue strip

---
+++ Form2.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TasksManager
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.ribbon1 = new System.Windows.Forms.Ribbon();
+            this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
+            this.tcTasksManager = new System.Windows.Forms.RibbonTab();
+            this.ribbonButton2 = new System.Windows.Forms.RibbonButton();
+            this.tcSubjectsManager = new System.Windows.Forms.RibbonTab();
+            this.ribbonPanel1 = new System.Windows.Forms.RibbonPanel();
+            this.ribbonPanel2 = new System.Windows.Forms.RibbonPanel();
+            this.btnCurrentTasks = new System.Windows.Forms.RibbonButton();
+            this.btnPreviousTasks = new System.Windows.Forms.RibbonButton();
+            this.tbForms = new MdiTabControl.TabControl();
+            this.SuspendLayout();
+            // 
+            // ribbon1
+            // 
+            this.ribbon1.CaptionBarVisible = false;
+            this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.Minimized = false;
+            this.ribbon1.Name = "ribbon1";
+            // 
+            // 
+            // 
+            this.ribbon1.OrbDropDown.BorderRoundness = 8;
+            this.ribbon1.OrbDropDown.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.OrbDropDown.Name = "";
+            this.ribbon1.OrbDropDown.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ribbon1.OrbDropDown.Size = new System.Drawing.Size(527, 447);
+            this.ribbon1.OrbDropDown.TabIndex = 0;
+            this.ribbon1.OrbText = "";
+            this.ribbon1.OrbVisible = false;
+            // 
+            // 
+            // 
+            this.ribbon1.QuickAccessToolbar.Items.Add(this.ribbonButton1);
+            this.ribbon1.QuickAccessToolbar.Items.Add(this.ribbonButton2);
+            this.ribbon1.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 9F);
+            this.ribbon1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ribbon1.Size = new System.Drawing.Size(800, 122);
+            this.ribbon1.TabIndex = 0;
+            this.ribbon1.Tabs.Add(this.tcTasksManager);
+            this.ribbon1.Tabs.Add(this.tcSubjectsManager);
+            this.ribbon1.Text = "ribbon1";
+            // 
+            // ribbonButton1
+            // 
+            this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
+            this.ribbonButton1.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.LargeImage")));
+            this.ribbonButton1.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
+            this.ribbonButton1.Name = "ribbonButton1";
+            this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
+            this.ribbonButton1.Text = "سيبسي";
+            // 
+            // tcTasksManager
+            // 
+            this.tcTasksManager.Name = "tcTasksManager";
+            this.tcTasksManager.Panels.Add(this.ribbonPanel1);
+            this.tcTasksManager.Panels.Add(this.ribbonPanel2);
+            this.tcTasksManager.Text = "إدارة المهام";
+            // 
+            // ribbonButton2
+            // 
+            this.ribbonButton2.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.Image")));
+            this.ribbonButton2.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.LargeImage")));
+            this.ribbonButton2.MaxSizeMode = System.Windows.Forms.RibbonElementSizeMode.Compact;
+            this.ribbonButton2.Name = "ribbonButton2";
+            this.ribbonButton2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.SmallImage")));
+            this.ribbonButton2.Text = "ribbonButton2";
+            // 
+            // tcSubjectsManager
+            // 
+            this.tcSubjectsManager.Name = "tcSubjectsManager";
+            this.tcSubjectsManager.Text = "إدارة المواد";
+            // 
+            // ribbonPanel1
+            // 
+            this.ribbonPanel1.ButtonMoreVisible = false;
+            this.ribbonPanel1.Items.Add(this.btnCurrentTasks);
+            this.ribbonPanel1.Name = "ribbonPanel1";
+            this.ribbonPanel1.Text = "";
+            // 
+            // ribbonPanel2
+            // 
+            this.ribbonPanel2.ButtonMoreEnabled = false;
+            this.ribbonPanel2.ButtonMoreVisible = false;
+            this.ribbonPanel2.Items.Add(this.btnPreviousTasks);
+            this.ribbonPanel2.Name = "ribbonPanel2";
+            this.ribbonPanel2.Text = "";
+            // 
+            // btnCurrentTasks
+            // 
+            this.btnCurrentTasks.Image = ((System.Drawing.Image)(resources.GetObject("btnCurrentTasks.Image")));
+            this.btnCurrentTasks.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnCurrentTasks.LargeImage")));
+            this.btnCurrentTasks.MinimumSize = new System.Drawing.Size(120, 0);
+            this.btnCurrentTasks.Name = "btnCurrentTasks";
+            this.btnCurrentTasks.SmallImage = ((System.Drawing.Image)(resources.GetObject("btnCurrentTasks.SmallImage")));
+            this.btnCurrentTasks.Text = "مهام حالية";
+            this.btnCurrentTasks.TextAlignment = System.Windows.Forms.RibbonItem.RibbonItemTextAlignment.Right;
+            this.btnCurrentTasks.Click += new System.EventHandler(this.btnCurrentTasks_Click);
+            // 
+            // btnPreviousTasks
+            // 
+            this.btnPreviousTasks.Image = ((System.Drawing.Image)(resources.GetObject("btnPreviousTasks.Image")));
+            this.btnPreviousTasks.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnPreviousTasks.LargeImage")));
+            this.btnPreviousTasks.MinimumSize = new System.Drawing.Size(120, 0);
+            this.btnPreviousTasks.Name = "btnPreviousTasks";
+            this.btnPreviousTasks.SmallImage = ((System.Drawing.Image)(resources.GetObject("btnPreviousTasks.SmallImage")));
+            this.btnPreviousTasks.Text = "مهام فائتة";
+            // 
+            // tbForms
+            // 
+            this.tbForms.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbForms.Location = new System.Drawing.Point(0, 122);
+            this.tbForms.MenuRenderer = null;
+            this.tbForms.Name = "tbForms";
+            this.tbForms.Size = new System.Drawing.Size(800, 384);
+            this.tbForms.TabCloseButtonImage = null;
+            this.tbForms.TabCloseButtonImageDisabled = null;
+            this.tbForms.TabCloseButtonImageHot = null;
+            this.tbForms.TabIndex = 1;
+            this.tbForms.TabsDirection = MdiTabControl.TabControl.FlowDirection.RightToLeft;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.tbForms);
+            this.Controls.Add(this.ribbon1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KeyPreview = true;
+            this.Name = "Form2";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "منظم التكاليف";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Ribbon ribbon1;
+        private System.Windows.Forms.RibbonButton ribbonButton1;
+        private System.Windows.Forms.RibbonButton ribbonButton2;
+        private System.Windows.Forms.RibbonTab tcTasksManager;
+        private System.Windows.Forms.RibbonPanel ribbonPanel1;
+        private System.Windows.Forms.RibbonButton btnCurrentTasks;
+        private System.Windows.Forms.RibbonPanel ribbonPanel2;
+        private System.Windows.Forms.RibbonButton btnPreviousTasks;
+        private System.Windows.Forms.RibbonTab tcSubjectsManager;
+        private MdiTabControl.TabControl tbForms;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasksManager
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        void _OpenForm(object sender)
+        {
+            Form frm = (Form)sender;
+            frm.BackColor = Color.White;
+
+            //string formTitle = frm.Text;
+
+            //// Check if a tab with the same form is already open
+            //foreach (TabPage tabPage in tbForms.TabPages)
+            //{
+            //    if (tabPage.Text == formTitle)
+            //    {
+            //        // If found, select the tab and return
+            //        ribbon1.do.SelectedTab = tabPage;
+            //        return;
+            //    }
+            //}
+            tbForms.TabPages.Add(frm);
+            frm.TopLevel = false; // Important to set this to false
+            frm.FormBorderStyle = FormBorderStyle.None; // Remove border
+            frm.Dock = DockStyle.None; // Ensure the form is not docked
+
+            // Add the form to the TabPage and show it
+
+
+            // Center the form within the TabPage
+
+            frm.Left = (tbForms.ClientSize.Width - frm.Width) / 2;
+            frm.Top = 15;
+            frm.Show();
+
+
+
+        }
+        private void btnCurrentTasks_Click(object sender, EventArgs e)
+        {
+            frmListTasks frm = new frmListTasks();
+            _OpenForm(frm);
+        }
+    }
+}
